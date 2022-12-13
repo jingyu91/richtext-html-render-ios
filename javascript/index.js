d518d1183adab90f4a9dbf9db9608c175d6cf1de
Jingyu Kim <jingyu91@naver.com>
2022-12-13 18:57:22 +0900

    Update index.js
    - add img tag if contains image asset in richtext

--- javascript/index.js
+++ javascript/index.js
@@ -1,8 +1,29 @@
 import { documentToHtmlString } from '@contentful/rich-text-html-renderer';
+import resolveResponse from 'contentful-resolve-response'
 
 export class Renderer {
-    static render(phrase) {
-        let result = documentToHtmlString(phrase)
-        return result
+  	static render(phrase) {
+    	let resolvedDocument = resolveResponse(phrase);
+
+    	const options = {
+		  renderNode: {
+		    'embedded-asset-block': (node) => {
+		      const url = node?.data?.target?.fields?.file?.url;
+
+		      if (typeof url === "undefined") {
+
+		      } else {
+		        return `<img src="https:${url}" alt="${node.data.target.fields.title}" />`;
+		      }
+		    },
+		  },
+		};
+
+    	let firstElement = resolvedDocument[0];
+		let fields = firstElement.fields;
+		let contentstext = fields.contentstext;
+		var result = documentToHtmlString(contentstext, options);
+
+    	return result
     }
 };
